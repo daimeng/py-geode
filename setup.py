b7eefc6c158e5dd5100ee6438d26101fc0127e80
daimeng <daimengchen@gmail.com>
2018-11-27 17:14:02 -0500

    setup.py submodule fix

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='geode',
-    version='0.1',
+    version='0.2',
     description='Geode',
     author='Test',
     author_email='daimengchen@gmail.com',
     url='',
-    packages=['geode'],
-    )
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+        'pandas',
+        'numpy',
+        'scipy',
+        'uvloop',
+        'ujson'
+    ])
